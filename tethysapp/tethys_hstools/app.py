0fed216bb4217e474e8759f469a20475560b08b5
Drew <zyli2004@gmail.com>
2015-04-02 16:32:29 -0600

    [#5] delete some printstatements and deprecated htmls

--- tethysapp/tethys_hstools/app.py
+++ tethysapp/tethys_hstools/app.py
@@ -20,16 +20,16 @@ class TethysToolsforHydroShare(TethysAppBase):
         UrlMap = url_map_maker(self.root_url)
 
         url_maps = (UrlMap(name='home',
-                           url='tethys-hstools/res/{res_id}/fn/{filename}',
+                           url='tethys-hstools/br/{branch}/res/{res_id}/fn/{filename}',
                            controller='tethys_hstools.controllers.restcall'),
 
                     UrlMap(name='home',
                            url='tethys-hstools/',
                            controller='tethys_hstools.controllers.home'),
 
-                    UrlMap(name='postexample',
-                           url='tethys-hstools/postexample',
-                           controller='tethys_hstools.controllers.postexample'),
+                    UrlMap(name='request_demo',
+                           url='tethys-hstools/request-demo',
+                           controller='tethys_hstools.controllers.request_demo'),
 
 
 
